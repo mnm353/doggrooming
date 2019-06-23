f568fe4cfa609977868daa389d48c8dbbad0e9cc
mnm353 <mnm353@gmail.com>
2019-06-23 13:27:36 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const massive = require('massive')
+const session = require('express-session')
+require('dotenv').config();
+
+const app = express();
+
+massive(process.env.CONNECTION_STRING)
+.then((dbInstance) =>{
+    app.set('db', dbInstance)
+    console.log('db is connected')
+})
+.catch((err)=>{
+    console.log('db not connected')
+})
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+
+const port = process.env.PORT || 8065;
+app.listen(port, () => {
+    console.log('Listening on port ${port}')
+})
+
