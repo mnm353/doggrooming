4dfb8c864614d6b0f0ba1cd605522ac63b6daac8
mnm353 <mnm353@gmail.com>
2019-07-11 14:24:59 -0600

    working out breaks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
 
-import { createStore, compose } from 'redux';
-import { Provider } from 'react-redux';
-import reducers from './redux/reducers/reducer';
+import { createStore, compose } from "redux"
+import { Provider } from "react-redux"
+//import reducers from './redux/reducers/reducer';
+import reducers from "./_reducers/reducer"
 
 //How to setup redux;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(reducers, composeEnhancers())
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
-
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+)
